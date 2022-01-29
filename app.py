dc983ae21e4ce8dd600a191bedf118c01d64feca
duypham228 <duy.cs.pham@gmail.com>
2022-01-29 17:22:57 -0600

    chatroom

--- app.py
+++ app.py
@@ -1,20 +1,40 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_socketio import SocketIO
 
 app = Flask(__name__, static_url_path='/static')
+app.config['SECRET_KEY'] = 'testingtesting'
+socketio = SocketIO(app)
 
+#GLOBAL ENV
+flight = "ABCD1234"
 
 @app.route('/')
-def hello():
+def home():
     return render_template('home.html')
 
 
+@app.route('/join', methods=["GET", "POST"])
+def join():
+    displayName = request.form.get("name", False)
+    if request.method == 'POST':
+        print(displayName)
+        return render_template("session.html", user=displayName)
+    else:
+        return render_template('join.html')
 
+@app.route('/session')
+def session():
+    return render_template('session.html')
 
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
 
-
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
 
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
