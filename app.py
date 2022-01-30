b679dedb3e84dc35246338808c7138b2a4658dcd
Chris <chris@client-10-228-41-13.tamulink.tamu.edu>
2022-01-30 10:42:47 -0600

    app time fix

--- app.py
+++ app.py
@@ -1,18 +1,26 @@
 from datetime import datetime
+from time import time
+from click import progressbar
 from flask import Flask, render_template, url_for, request
 import requests
 import json
+import pyowm
 
 app = Flask(__name__, static_url_path='/static')
 now = datetime.now()
 date = now.strftime("%Y")+"-"+now.strftime("%m")+"-"+now.strftime("%d")
-test_date = '2022-01-29'
+test_date = '2022-01-30'
 params = {
-  'access_key': '6982c8f3d7c66ea30604f89bfbc58608'
+  'access_key': 'ca97067f353996f37afa2117c9107b37'
 }
 flight_information  = {}
 
+weather_key = "1762906f52427514c462d643654533b6"
+owm = pyowm.OWM(weather_key)
+
+
 def strip_time_string(time_string):
+    print(time_string)
     time_string = (time_string.split("T")[1]).split("+")[0]
     time_string = datetime.strptime(time_string,"%H:%M:%S")
     return time_string
@@ -39,15 +47,26 @@ def start():
         print(params)
         api_result = requests.get('http://api.aviationstack.com/v1/flights', params)
         api_response = api_result.json()
+        print(api_response)
         global flight_information
         for flights in api_response["data"]:
             if flights["flight_date"] == test_date:
                 flight_information = flights
+        global arrival
         hidden = "" if flight_information is not None else "is-hidden"
         arrival = (strip_time_string(flight_information["arrival"]["estimated"])) - calc_now_time()
         print("arrival "+str(arrival))
-        flight_information["departure"]["scheduled"] = datetime.strftime(strip_time_string(flight_information["departure"]["scheduled"]),"%H:%M:%S")
-        flight_information["arrival"]["estimated"] = datetime.strftime((strip_time_string(flight_information["arrival"]["estimated"])),"%H:%M:%S")
+        print(flight_information["arrival"]["estimated"] )
+        print("arrival "+str(arrival))
+        time1 = strip_time_string(flight_information["departure"]["scheduled"])
+        time2 = strip_time_string(flight_information["arrival"]["estimated"])
+        t = time2.time()
+        seconds = (t.hour * 60 + t.minute) * 60 + t.second
+        global progressvalue
+        progressvalue = round(((time2-calc_now_time()).total_seconds()/(time2-time1).total_seconds()))*100
+        print(progressvalue)
+        flight_information["departure"]["scheduled"] = datetime.strftime(time1,"%H:%M:%S")
+        flight_information["arrival"]["estimated"] = datetime.strftime(time2,"%H:%M:%S")
         print(flight_information["arrival"]["estimated"] )
         print(flight_information)
         return render_template('index.html', flight_information=flight_information, hidden = hidden, placeholder = params["flight_iata"], arrival = arrival)
@@ -57,7 +76,13 @@ def start():
     
 @app.route('/dashboard', methods = ["GET"])
 def dashboard():
-    return render_template("dashboard.html", flight_information=flight_information)
+    return render_template("dashboard.html", flight_information=flight_information, arrival = arrival, progressvalue=progressvalue)
+
+@app.route('/guide', methods = ["GET"])
+def guide():
+    # observation = owm.weather_at_place()
+    params["flight_iata"]
+    return render_template("guide.html", flight_information=flight_information)
 
 if __name__ == "__main__":
     app.run(debug=True)
