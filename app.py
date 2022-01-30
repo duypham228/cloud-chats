9d643e30adbe3f5e6c2ca34275af7c03261f075a
duypham228 <duy.cs.pham@gmail.com>
2022-01-30 08:25:07 -0600

    Integrate Chat Services

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from datetime import datetime
 from flask import Flask, render_template, url_for, request
+from flask_socketio import SocketIO
 import requests
 import json
 import pyowm
+import googlemaps
 
 app = Flask(__name__, static_url_path='/static')
+socketio = SocketIO(app)
 now = datetime.now()
 date = now.strftime("%Y")+"-"+now.strftime("%m")+"-"+now.strftime("%d")
 test_date = '2022-01-30'
@@ -15,6 +18,11 @@ flight_information  = {}
 
 weather_key = "1762906f52427514c462d643654533b6"
 owm = pyowm.OWM(weather_key)
+mgr = owm.weather_manager()
+
+google_key = "AIzaSyDtNE7L1vc3I8ZU8emZdAMFoZJeplm1ypY"
+gmaps = googlemaps.Client(key = google_key)
+
 
 
 def strip_time_string(time_string):
@@ -34,7 +42,7 @@ def find_city(iata):
     "key":"c71fff2ce5",
     "secret": "b109d0741b40414"
     }
-    r = requests.get("https://www.air-port-codes.com/api/v1/single?iata=IAH",params=params)
+    r = requests.get("https://www.air-port-codes.com/api/v1/single?iata=" + iata,params=params)
     return r
 
 @app.route('/', methods = ["GET","POST"])
@@ -69,11 +77,73 @@ def dashboard():
     # print(flight_information)
     return render_template("dashboard.html", flight_information=flight_information)
 
+@app.route('/join', methods=["GET", "POST"])
+def join():
+    displayName = request.form.get("name", False)
+    if request.method == 'POST':
+        print(displayName)
+        return render_template("flightchat.html", user=displayName)
+    else:
+        return render_template('join.html')
+
+@app.route('/flightchat')
+def flightchat():
+    return render_template('flightchat.html')
+
+def messageReceived(methods=['GET', 'POST']):
+    print('message was received!!!')
+
+@socketio.on('my event')
+def handle_my_custom_event(json, methods=['GET', 'POST']):
+    print('received my event: ' + str(json))
+    socketio.emit('my response', json, callback=messageReceived)
+
 @app.route('/guide', methods = ["GET"])
 def guide():
-    # observation = owm.weather_at_place()
-    params["flight_iata"]
-    return render_template("guide.html", flight_information=flight_information)
+    # prepare info for requests
+    iata = flight_information["arrival"]["iata"]
+    r = find_city(iata)
+    city = (r.json())["airport"]["city"]
+    lat = (r.json())["airport"]["latitude"]
+    lng = (r.json())["airport"]["longitude"]
+    location = lat + "," + lng
+
+    # Weather Forecast guide
+    observation = mgr.weather_at_place(city)
+    w = observation.weather
+
+    status = w.status
+    dstatus = w.detailed_status         # 'clouds'
+    wind = w.wind()                  # {'speed': 4.6, 'deg': 330}
+    humidity = w.humidity                # 87
+    temp = w.temperature('fahrenheit')  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+    visibility = w.visibility_distance
+    # # icon = "http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"
+    # print(w.icon)
+    # forecast = mgr.forecast_at_place(city, 'daily')
+    # # print(forecast.when_clear())
+
+
+    # Restaurant guide
+    restaurants_result  = gmaps.places_nearby(location=location, radius = 10000, type="restaurant")
+    restaurants = restaurants_result["results"]
+    # res_photos = []
+    # for restaurant in restaurants:
+    #     photo_ref = restaurant["photos"][0]["photo_reference"]
+    #     url = "https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photo_reference=" + photo_ref + "&key=" + google_key
+    #     res_photos += url
+
+    # Point of interest guide
+    pois_result  = gmaps.places_nearby(location=location, radius = 10000, type="point_of_interest")
+    pois = pois_result["results"]
+
+    weather = {"city": city, "status": status, "dstatus": dstatus, "wind": wind, "humidity": humidity, "temp": temp, "visibility": visibility}
+    # print(weather)
+    return render_template("guide.html", weather=weather, restaurants=restaurants, pois=pois)
+
+@app.route('/faq', methods = ["GET"])
+def faq():
+    return render_template("faq.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    socketio.run(app, debug=True)
